9bc124f3d77348af967537add60c9d4d14ec0c23
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-03-24 10:20:18 +0600

    Operators

---
+++ 11-operators/script.js
@@ -0,0 +1,42 @@
+// math operators
+
+// addition
+console.log(1 + 2);
+
+// subtraction
+console.log(3 - 1);
+
+// multiplication
+console.log(2 * 3);
+
+// division
+console.log(6 / 2);
+
+// remainder
+console.log(5 % 2);
+
+// exponentiation
+console.log(2 ** 3);
+
+// concatenation
+console.log("Hello, " + "World!");
+console.log("1" + 2);
+
+// unary plus
+let x = "1";
+console.log(+x);
+
+// assignment
+let y = 2;
+y += 3;
+console.log(y);
+
+// increment
+let z = 5;
+z++;
+console.log(z);
+
+// decrement
+let a = 5;
+a--;
+console.log(a);
