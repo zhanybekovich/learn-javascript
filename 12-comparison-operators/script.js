196e69cc67f9048fd4ba359ec87ecb833bdff8f1
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-03-24 10:25:54 +0600

    Comparison operators

---
+++ 12-comparison-operators/script.js
@@ -0,0 +1,29 @@
+// greater
+console.log(5 > 3);
+
+// less
+console.log(5 < 3);
+
+// greater or equal
+console.log(5 >= 3);
+
+// less or equal
+console.log(5 <= 3);
+
+// equal
+console.log(5 == 3);
+console.log(5 == "5");
+
+// strict equal
+console.log(5 === 3);
+console.log(5 === "5");
+
+// not equal
+console.log(5 != 3);
+console.log(5 != "5");
+
+// strict not equal
+console.log(5 !== 3);
+console.log(5 !== "5");
+
+// all comparison operators return a boolean value
