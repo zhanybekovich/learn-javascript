08e362339100205b6032ab16600b2a89d589aec6
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2025-03-24 10:00:02 +0600

    Alert, prompt, confirm

---
+++ 09-alert-promt-confirm/script.js
@@ -0,0 +1,14 @@
+// alert for showing messages
+alert("hello, world!");
+
+// prompt for getting user input
+let username = prompt("What is your name?");
+alert(`Hello, ${username}!`);
+
+// prompt with default value
+let username2 = prompt("What is your name?", "John");
+alert(`Hello, ${username2}!`);
+
+// confirm for getting user confirmation
+let isAgree = confirm("Do you agree?");
+alert(isAgree); // true or false
